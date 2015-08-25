90fcd752d5202d821544db77d952e07a4c451737
PhearZero <personalpc@live.com>
2015-08-24 21:39:49 -0500

    Updating ignore, updating wordpress

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
+var config = {
 
-
+};
 /**
  * Grunt!
  */
@@ -7,7 +8,25 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        //TODO: Setup Grunt tasks!
+        "db_dump":{
+            "production": {
+                "options": {
+                    "title": "Production DB",
+
+                    "database": "db_name",
+                    "user": "db_username",
+                    "pass": "db_password",
+                    "host": "db_host",
+
+                    "ssh_host": "db_ssh_host",
+
+                    "backup_to": "/db/backups/production.sql"
+                }
+            }
+        }
+
     });
 
     grunt.loadNpmTasks('grunt-phplint');
